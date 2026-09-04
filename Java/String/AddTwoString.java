5cbaedcba1b548d512ab90d41ee80e2a93b4a096
harshadj5 <harshadj063@gmail.com>
2026-09-04 22:18:46 +0530

    update file

--- Java/String/AddTwoString.java
+++ Java/String/AddTwoString.java
@@ -3,7 +3,7 @@ class AddTwoString{
     public static void main(String[]args){
         String str = "good";
         String str1 = "day";
-        String str 2 = str.concat(str1);
+        String str2 = str.concat(str1);
         System.out.println("String is"+str2);
     }
 }
